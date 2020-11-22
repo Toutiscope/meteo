aa32e0a559978376be2ca9b6f048e81fecc630bc
Violette <violette.hurel@gmail.com>
2020-11-22 18:13:44 +0100

    Add last modifications

--- js/vue.js
+++ js/vue.js
@@ -1,6 +1,6 @@
 // https://api-adresse.data.gouv.fr/search/?q=nantes&type=municipality
 
-// URL to the real API, but limited requests per night
+// URL to the real API, but limited requests per isNight
 const siteURL = "http://api.openweathermap.org/data/2.5/weather?zip=";
 const keyAPI =
   ",fr&units=metric&lang=fr&appid=482d9c84ad6a948e60a12ad4b71979f1";
@@ -8,6 +8,8 @@ const keyAPI =
 // Alternative local URL for developpment
 const jsonFile = "js/openweather";
 const jsonExt = ".json";
+
+let day;
 
 /**
  * Weather App - Violette Hurel
@@ -19,7 +21,7 @@ var demo = new Vue({
   data: {
     display: false,
     error: false,
-    night: "",
+    isNight: "",
     show_input: false,
     zipcode: 44300,
     apiURL: siteURL.concat(44300, keyAPI),
@@ -39,13 +41,22 @@ var demo = new Vue({
         .then(function (response) {
           console.log(response);
           self.response = response.data;
+          // self.response = "Erreur de chargement"; // L'afficher sur la page
           self.error = false;
           self.display = true;
+
+          self.week = ["Dimanche", "Lundi", "Mardi", "Mercredi", "Jeudi", "Vendredi", "Samedi"];
+          self.day = new Date(response.data.dt * 1000).getDay();
+          self.date = new Date(response.data.dt * 1000).getDate();
+          self.month = new Date(response.data.dt * 1000).getMonth();
+          self.year = new Date(response.data.dt * 1000).getFullYear();
+
+          self.hours = new Date(response.data.dt * 1000).getHours();
+          self.minutes = new Date(response.data.dt * 1000).getMinutes();
         })
         .catch(function (error) {
           console.log(error);
           self.error = true;
-          // self.response = "Erreur de chargement"; // L'afficher sur la page
         });
     },
 
@@ -64,16 +75,16 @@ var demo = new Vue({
         this.response.dt <= this.response.sys.sunrise &&
         this.response.dt >= this.response.sys.sunset
       ) {
-        this.night = true;
-        console.log(this.night);
+        this.isNight = true;
+        console.log(this.isNight);
       } else {
-        this.night = false;
-        console.log(this.night);
+        this.isNight = false;
+        console.log(this.isNight);
       }
     },
 
     toggleNight() {
-      this.night = !this.night;
+      this.isNight = !this.isNight;
     },
 
     refresh() {
